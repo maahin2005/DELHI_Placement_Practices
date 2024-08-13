a7bac4cc07311f79142b29b38d3fe14b5769c55a
maahin2005 <mahin.malek2005@gmail.com>
2024-08-13 23:48:53 +0530

    edit title and remove():functionality

--- todo-vanilla/script.js
+++ todo-vanilla/script.js
@@ -34,17 +34,43 @@ const handleChange = (id) => {
   addTodo();
 };
 
+const handleDeleteTodo = (id) => {
+  todoArr = todoArr.filter((todo) => todo.id !== id);
+  localStorage.setItem("todos", JSON.stringify(todoArr));
+  addTodo();
+};
+
+const handlesubmitEdit = (id, newTitle) => {
+  todoArr = todoArr.map((todo) => {
+    if (todo.id === id) {
+      todo.title = newTitle;
+    }
+    return todo;
+  });
+  localStorage.setItem("todos", JSON.stringify(todoArr));
+  addTodo();
+};
+
 function addTodo() {
   showTodosDiv.innerHTML = "";
   todoArr.forEach((todo) => {
     showTodosDiv.innerHTML += `
-      <div class="todo-container p-2">
-        <h1>Title: ${todo.title}</h1>
-        <h1>Status: ${todo.status}</h1>
-         <label>Status:<input type="checkbox" ${
-           todo.status ? "checked" : ""
-         } onchange="handleChange(${todo.id})"/>
-          </label>
+      <div class="todo-container col-sm-3 p-2">
+        <h1 class="fs-3">Title: ${todo.title}</h1>
+        <h1 class="fs-5">Status: ${todo.status}</h1>
+        <label>Status:<input type="checkbox" ${
+          todo.status ? "checked" : ""
+        } onchange="handleChange(${todo.id})"/>
+        </label>
+        <button class="btn btn-outline-danger" onclick="handleDeleteTodo(${
+          todo.id
+        })">Delete</button>
+        <form onsubmit="event.preventDefault(); handlesubmitEdit(${
+          todo.id
+        }, this.newTitle.value);">
+          <input placeholder="new title" name="newTitle"/>
+          <button class="btn btn-outline-info" type="submit">Edit Title</button>
+        </form>
       </div>
     `;
   });
